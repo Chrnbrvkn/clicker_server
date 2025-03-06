a3d65b64cf790b4106acfc12eae7223d7efd012e
Nikita <proshaiwow@gmail.com>
2025-03-06 07:38:00 +0400

    add logs

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,8 +1,9 @@
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const generateToken = (user) => {
-  console.log('jwtUtils.js ' + JSON.stringify(user));
+  console.log("jwtUtils.js " + JSON.stringify(user));
   return jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET);
 };
 
